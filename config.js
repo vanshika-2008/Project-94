5410f35becee4cc9cc0c949fb0913ae633b3b8ff
vanshika-2008 <70143660+vanshika-2008@users.noreply.github.com>
2021-07-29 14:57:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+ export const firebaseConfig = {
+    apiKey: "AIzaSyDnTWPdQvouemveLJuuoGWnY9NLiMgxkjI",
+    authDomain: "be-loved--1.firebaseapp.com",
+    databaseURL: "https://be-loved--1-default-rtdb.firebaseio.com",
+    projectId: "be-loved--1",
+    storageBucket: "be-loved--1.appspot.com",
+    messagingSenderId: "790906100519",
+    appId: "1:790906100519:web:eb795896278bded1e66702"
+  };
